fef17f6b49f6298ed3f4a0a5353209676cef42bd
DOES <ldk980130@gmail.com>
2022-10-10 17:43:44 +0900

    Initial commit

---
+++ src/main/java/com/example/loadbalancedexample/LoadBalancedExampleApplication.java
@@ -0,0 +1,13 @@
+package com.example.loadbalancedexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoadBalancedExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoadBalancedExampleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/loadbalancedexample/LoadBalancedExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.loadbalancedexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoadBalancedExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
